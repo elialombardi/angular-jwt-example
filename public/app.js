34f5bcef5b903ea31d12e99bf9531c4fa89c3e11
Elia Lombardi <elia5588@gmail.com>
2016-07-16 12:22:51 +0200

    Login form added

--- public/app.js
+++ public/app.js
@@ -4,10 +4,22 @@
 
 	app.constant('API_URL', 'http://localhost:3000');
 
-	app.controller('MainCtrl', function(RandomUserFactory) {
+	app.controller('MainCtrl', function(RandomUserFactory, UserFactory) {
 		'use strict';
 		var vm = this;
 		vm.getRandomUser = getRandomUser;
+		vm.login = login;
+
+		function login(username, password) {
+			UserFactory.login(username, password).then(function success(res) {
+				delete vm.errorMessage;
+				vm.user = res.data;
+			}, errorHandler);
+		}
+
+		function errorHandler(response) {
+			vm.errorMessage='Error: ' + response.data;
+		}
 
 		function getRandomUser() {
 			RandomUserFactory.getUser().then(function success(res) {
@@ -26,4 +38,19 @@
 			return $http.get(API_URL + '/random-user');
 		}
 	});
+
+	 app.factory('UserFactory', function UserFactory($http, API_URL) {
+	 	'use strict';
+	 	return {
+	 		login: login
+	 	};
+
+	 	function login(username, password) {
+			console.log(username, password)			
+	 		return $http.post(API_URL + '/login', {
+	 			username: username,
+	 			password: password
+	 		});
+	 	}
+	 })
 })();
--- server.js
+++ server.js
@@ -1,18 +1,43 @@
 var express = require('express');
 var faker = require('faker');
 var cors = require('cors');
+var bodyParser = require('body-parser');
+
+var user = {
+	username: 'elia',
+	password: 'elia'
+}
 
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/random-user', function(req, res) {
-	console.log(faker);
 	var user = faker.helpers.userCard();
 	user.avatar = faker.image.avatar();
 	res.json(user);
 });
 
+app.post('/login', authenticate, function(req, res) {
+	res.send(user); 
+});
+
 app.listen(3000, function() {
 	console.log('App listening on localhost:3000');
 })
+
+
+
+// UTIL FUNCTIONS
+function authenticate(req, res, next) {
+	var body = req.body;
+
+	if(!body.username || !body.password) {
+		res.status(400).end('Must provide username and password');
+	} else if(body.username != user.username || body.password != user.password) {
+		res.status(400).end('Invalid username and password');
+	}
+
+	next();
+}
